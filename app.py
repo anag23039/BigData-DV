7dfce76f14bc336d729c4f2070626aded6d58226
ageorgievani <sbs23039@student.cct.ie>
2023-11-18 11:32:14 +0000

    Dashboard files

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
+
+st.title(" :bar_chart: Sample SuperStore EDA")
+
+
+# In[ ]:
+
+
+
+
